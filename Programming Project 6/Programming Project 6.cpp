1e90d51b03a95d7cf21c8d3a34a96befd937139d
andsprague1 <andsprague1@gmail.com>
2025-11-10 18:10:57 -0500

    Initial commit

--- Programming Project 6/Programming Project 6.cpp
+++ Programming Project 6/Programming Project 6.cpp
@@ -2,22 +2,85 @@
 //
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
+void getCostAndMarkup(double&, int&);
+int displayPrice(double);
+bool getQuit();
+
 int main()
 {
-    cout << "Hello World!\n";
+
+    bool contin=true;
+    while (contin)
+    {
+        double cost;
+        int percentMarkup;
+        getCostAndMarkup(cost, percentMarkup);
+        double retail = cost * (1 + (percentMarkup / 100.0));
+
+        displayPrice(retail);
+
+        contin = getQuit();
+    }
+
     return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+/// <summary>
+/// The purpose of this function is to get the cost and markup
+/// </summary>
+/// <param name="cost">The cost of the product</param>
+/// <param name="markup">The markup on the product</param>
+void getCostAndMarkup(double& cost, int& markup)
+{
+    cout << "Please enter the cost of the product:";
+    cin >> cost;
+    cout << "Please enter the markup of the product:";
+    cin >> markup;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+/// <summary>
+/// This function should display the price of the product
+/// </summary>
+/// <param name="price">The price to display</param>
+/// <returns>error codes
+/// 0 is good
+/// 1 is negative price
+/// 2 is impossible I'm coding for that</returns>
+int displayPrice(double price)
+{
+    if (price < 0)
+    {
+        cout << "Error";
+        return 1;
+    }
+
+    cout<< "The retail price is:" << price<<"$" << endl;
+
+    return 0;
+}
+
+/// <summary>
+/// Check to see if the user wants to continue
+/// </summary>
+/// <returns>True for continue, False for quit</returns>
+bool getQuit()
+{
+    char c;
+
+    cout << "Do you want to continue (y/n)";
+    cin >> c;
+
+    if (c == 'y')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
